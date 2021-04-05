73ab31aaf2bc15726fd663f911535b92ed9cbc11
Vailorium <casterthe5587@gmail.com>
2021-04-05 17:34:52 +1200

    v033 update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { ReportBugDialog } from './pages/report-bug-dialog/report-bug-dialog';
 export class AppComponent {
   title = 'Askrs Archives';
 
-  version = "0.3.1";
+  version = "0.3.3";
 
   constructor(private dialog: MatDialog){
   }
