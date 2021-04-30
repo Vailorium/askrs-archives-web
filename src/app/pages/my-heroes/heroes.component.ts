15a98950c49dd1b952bb6ef3dc90bc8e568e59d2
Vailorium <casterthe5587@gmail.com>
2021-04-30 18:25:52 +1200

    changes

--- src/app/pages/my-heroes/heroes.component.ts
+++ src/app/pages/my-heroes/heroes.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { Observable } from 'rxjs';
 import { BuildModel, HeroInfoModel } from 'src/app/models';
 import { BuildService } from 'src/app/services/build-service';
+import { ConfirmDialog } from '../confirm-dialog/confirm-dialog';
 
 @Component({
   selector: 'app-heroes',
@@ -15,7 +18,7 @@ export class HeroesComponent implements OnInit {
   public time = new Date().getTime();
 
   filter: FormControl = new FormControl('');
-  constructor(private buildService: BuildService) {
+  constructor(private buildService: BuildService, private dialog: MatDialog) {
     this.builds = this.buildService.convertFromAPIBuilds([{buildName: "Test", unitId: "PID_マルス", visible: false, rarity: 5, merges: 0, skills: {}, resplendent: false, ivs: {boon: 0, bane: 0}, dragonflowers: 0, blessing: 0, allySupport: {rank: 0}, summonerSupport: 0},
   {buildName: "Test 2", unitId: "PID_ティアモ", visible: true, rarity: 5, merges: 0, skills: {}, resplendent: true, ivs: {boon: 0, bane: 0}, dragonflowers: 0, blessing: 0, allySupport: {rank: 0}, summonerSupport: 0},
   {buildName: "Test 2", unitId: "PID_ティアモ", visible: true, rarity: 5, merges: 0, skills: {}, resplendent: true, ivs: {boon: 0, bane: 0}, dragonflowers: 0, blessing: 0, allySupport: {rank: 0}, summonerSupport: 0},
@@ -39,6 +42,18 @@ export class HeroesComponent implements OnInit {
 
   getBuilds(){
 
+  }
+
+  deleteBuild(buildIndex: number){
+    let confirm = this.dialog.open(ConfirmDialog, {data: {message: "Are you sure you want to delete this build?", title: "Confirm Clearing", default: false, options: [{display: "Cancel", color: "", value: false}, {display: "Clear", color: "warn", value: true}]}});
+  
+    confirm.afterClosed().subscribe((res: boolean) => {
+      if(res === true){
+        // Delete via API
+        // .then()
+        this.builds.splice(buildIndex, 1);
+      }
+    });
   }
 
 }
