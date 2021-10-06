54fa6114d575cd5b42956e6f6b6385b5f9f7f234
Samuel <casterthe5587@gmail.com>
2021-10-06 17:47:56 +1300

    v0321 update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { ReportBugDialog } from './pages/report-bug-dialog/report-bug-dialog';
 export class AppComponent {
   title = 'Askrs Archives';
 
-  version = "0.3.20";
+  version = "0.3.21";
 
   constructor(private dialog: MatDialog){
   }
